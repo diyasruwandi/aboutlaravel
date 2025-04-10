65c19687eb1bae79494758786910922ff50511e9
Diyas <diyasruwandi273@gmail.com>
2025-04-10 21:29:52 +0700

    [Pertemuan 4] Membuat Controller dan Model

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\HomepageController;
 
-Route::get('/', function () {
-    return view('web.homepage');
-});
+// Route::get('/', function () {
+//     return view('web.homepage');
+// });
 
-Route::get('/beauty', function () {
-    return view('beauty');
-});
+Route::get('/',[HomepageController::class,'index']);
+Route::get('products', [HomepageController::class, 'products']);
+Route::get('product/{slug}', [HomepageController::class, 'product']);
+Route::get('categories',[HomepageController::class, 'categories']);
+Route::get('category/{slug}', [HomepageController::class, 'category']);
+Route::get('cart', [HomepageController::class, 'cart']);
+Route::get('checkout', [HomepageController::class, 'checkout']);
 
-Route::get('products', function () {
-    return view('web.products');
-});
 
-Route::get('product/{slug}', function () {
-    return "INI ADALAH HALAMAN SINGLE PRODUK -".$slug;
-});
 
-Route::get('categories', function () {
-    return "INI ADALAH HALAMAN KATEGORI PRODUK ";
-});
+// Route::get('/beauty', function () {
+//     return view('beauty');
+// });
 
-Route::get('category/{slug}', function () {
-    return "INI ADALAH HALAMAN SINGLE KATEGORI - ".$slug;
-});
+// Route::get('products', function () {
+//     return view('web.products');
+// });
 
-Route::get('cart', function () {
-    return "INI ADALAH HALAMAN CART ";
-});
+// Route::get('product/{slug}', function () {
+//     return "INI ADALAH HALAMAN SINGLE PRODUK -".$slug;
+// });
+
+// Route::get('categories', function () {
+//     return "INI ADALAH HALAMAN KATEGORI PRODUK ";
+// });
+
+// Route::get('category/{slug}', function () {
+//     return "INI ADALAH HALAMAN SINGLE KATEGORI - ".$slug;
+// });
+
+// Route::get('cart', function () {
+//     return "INI ADALAH HALAMAN CART ";
+// });
 
 // Route::get('/', function () {
 //     return view('welcome');
