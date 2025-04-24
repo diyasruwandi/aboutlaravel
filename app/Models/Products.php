b04743208bd0c3984c6beb856712e5045b0b4657
Diyas <diyasruwandi273@gmail.com>
2025-04-24 12:59:32 +0700

    [Database - Menambahkan tabel Products]

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
-class Categories extends Model
+class Products extends Model
 
 {
     use HasFactory;
-    protected $table = 'product_categories';
+    protected $table = 'Products';
        
 }
